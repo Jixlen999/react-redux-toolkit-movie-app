567ec96b59bf0ef329297d792f9259da6503d57c
Victoria <viktoriyaivanyutenko@yandex.ru>
2022-08-13 11:33:20 +0300

    Main functionality finished. Cards added, search added, slider added.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,17 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncShows } from "../../features/movies/movieSlice";
 
 const Home = () => {
 	const dispatch = useDispatch();
-	const movieText = "Harry";
+	const movieText = "Spider";
+	const showText = "Friends";
 	useEffect(() => {
-		const fetchMovies = async () => {
-			const response = await movieApi
-				.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-				.catch((err) => {
-					console.log("Error:", err);
-				});
-			dispatch(addMovies(response.data));
-		};
-		fetchMovies();
-	}, []);
+		dispatch(fetchAsyncMovies(movieText));
+		dispatch(fetchAsyncShows(showText));
+	}, [dispatch]);
 	return (
 		<div className="banner-img">
 			<MovieListing />
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
+import Slider from "react-slick";
+import { settings } from "../../common/settings";
 
 const MovieListing = () => {
 	const movies = useSelector(getAllMovies);
+	const shows = useSelector(getAllShows);
 	let renderMovies = "";
+	let renderShows = "";
 
 	renderMovies =
 		movies.Response === "True" ? (
@@ -18,11 +22,30 @@ const MovieListing = () => {
 				<h3>{movies.Error}</h3>
 			</div>
 		);
+
+	renderShows =
+		shows.Response === "True" ? (
+			shows.Search.map((show, index) => {
+				return <MovieCard key={index} data={show}></MovieCard>;
+			})
+		) : (
+			<div className="movies-error">
+				<h3>{movies.Error}</h3>
+			</div>
+		);
 	return (
 		<div className="movie-wrapper">
 			<div className="movie-list">
 				<h2>Movies</h2>
-				<div className="movie-container">{renderMovies}</div>
+				<div className="movie-container">
+					<Slider {...settings}>{renderMovies}</Slider>
+				</div>
+			</div>
+			<div className="show-list">
+				<h2>Shows</h2>
+				<div className="movie-container">
+					<Slider {...settings}>{renderShows}</Slider>
+				</div>
 			</div>
 		</div>
 	);
